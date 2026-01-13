e6777ee411ab3b9f302b2124a0d54152f42cd862
Melissa Destez <melissadestez@gmail.com>
2026-01-13 19:59:24 -0300

    Exercise 3 W1 Stretch Challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,29 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int random = randomGenerator.Next(1, 11);
+        int random = randomGenerator.Next(1, 100);
         int number = 0;
+        string answer = "";
+        int guess = 0;
+        
 
         do
         {
-        Console.Write("What's you guess? ");
-        string numberInput = Console.ReadLine();
-        number = int.Parse(numberInput);
+            do
+            {
 
-        if (number < random)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (number > random)
-        {
-            Console.WriteLine("Lower");
-        }   
-        } while (number != random);
+            Console.Write("What's you guess? ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
 
-        if (number == random)
-        {
-            Console.WriteLine("You guess it!");
-        }
+            if (number < random)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number > random)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            guess++;
+
+            } while (number != random);
+
+            if (number == random)
+            {
+                Console.WriteLine($"You guess it! You made in {guess} guesses!");
+                Console.Write("Would you like to play again? ");
+                answer = Console.ReadLine();
+            }
+
+        } while (answer == "yes");
         
     }
 }
