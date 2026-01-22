2ca52d5610966b8e67571ce20573fd8128a7d56a
Melissa Destez <melissadestez@gmail.com>
2026-01-21 23:10:34 -0300

    Entry.cs Display part

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -3,16 +3,9 @@ public class Entry
     public string _date;
     public string _promptText;
     public string _entryText;
-
     public void Display()
     {
-        DateTime theCurrentTime = DateTime.Now;
-        string dateText = theCurrentTime.ToShortDateString();
-
-        string filename = "myFile.txt";
-        using (StreamWriter outputFile = new StreamWriter(filename, true))
-        {
-        outputFile.WriteLine($"");
-        }
+        Console.Write($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}");
     }
 }
