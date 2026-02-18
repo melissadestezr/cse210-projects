46743e5b5af1ac7aa64ffa3f74ea49e56806bc62
Melissa Destez <melissadestez@gmail.com>
2026-02-18 20:06:58 -0300

    W7 Tracking Program

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,27 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    protected int GetMinutes() => _minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():F1} miles, " +
+               $"Speed: {GetSpeed():F1} mph, " +
+               $"Pace: {GetPace():F2} min per mile";
+    }
+}
